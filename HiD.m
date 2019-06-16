567ecd57ce870865a4f3d33d7cef822fb46e8418
romanbarth <37847173+romanbarth@users.noreply.github.com>
2019-06-16 21:34:11 +0200

    made platform-independent

--- HiD.m
+++ HiD.m
@@ -3,19 +3,19 @@
 % ----------------------------------------------------------------------- %
 %
 %
-% Demo for example data (U2OS cells labeled by DNA-SiR of size 
-% 358 pixels x 246 pixels x 150 frames).
-% The data is a part of the entire dataset of size 
+% Demo for example data (U2OS cells labeled by DNA-SiR of size
+% 310 pixels x 213 pixels x 150 frames).
+% The data is a part of the entire dataset of size
 % 1024 pixels x 1024 pixels x 150 frames (65 nm pixel size and 5 fps).
 %
 % The example data takes ~2h 20 min to process on following system:
 %       Intel(R) Core(TM) i7-6700IQ CPU @ 2.60 GHz 2.60 Ghz
 %       16.0 Gb RAM
 %
-% developed at:  
-%       Laboratoire de Biologie Moléculaire Eucaryote (LBME), 
-%       Centre de Biologie Intégrative (CBI), CNRS; 
-%       University of Toulouse, UPS; 31062 
+% developed at:
+%       Laboratoire de Biologie Moléculaire Eucaryote (LBME),
+%       Centre de Biologie Intégrative (CBI), CNRS;
+%       University of Toulouse, UPS; 31062
 %       Toulouse; France
 %
 % ----------------------------------------------------------------------- %
@@ -25,7 +25,12 @@ clearvars
 
 %% Set parameters
 % give the path to the file to analyse
-directory = [pwd '\U2OS_DNA_Serum_example_data.tif'];
+filepath = pwd;
+if ispc
+    directory = [filepath '\U2OS_DNA_Serum_example_data.tif'];
+else
+    directory = [filepath '/U2OS_DNA_Serum_example_data.tif'];
+end
 
 % pixel size in micrometer
 pixelsize= 0.065;
@@ -66,6 +71,13 @@ MSD = MSDcalculation(xp, yp, mask_nuc, dT);
 
 %% Bayesian inference for MSD classification and regression
 Bayes = MSDBayes(MSD, dT);
+
+%% save data
+if ispc
+    save([filepath '\HiD_Analysis_' convertDate(datee) '.mat'])
+else
+    save([filepath '/HiD_Analysis_' convertDate(datee) '.mat'])
+end
 
 %% plot resulting parameter maps: fluorescence image, model selection, D, alpha, V
 PlotMaps(im, mask_nuc, pixelsize, Bayes)
