50d616b830b77a9fc0a795c5a7653591c299b294
romanbarth <37847173+romanbarth@users.noreply.github.com>
2020-02-03 09:01:08 +0100

    Update Preprocessing.m

--- Code/Preprocessing.m
+++ Code/Preprocessing.m
@@ -1,13 +1,13 @@
 %
-% Dense Flow reConstruction and Correlation (DFCC)
+% Hi-D
 % ----------------------------------------------------------------------- %
 %
 % Reference to the publication:
-%   Shaban, H.A.; Barth, R.; Bystricky, K. title. Nucleic Acids Research, volume(issue):pages1-pages2, 2018
+%   Shaban, H.; Barth, R.; Recoules, L.; Bystricky, K.; Hi-D: Nanoscale mapping of nuclear dynamics in single living cells. Genome Biology (2020)
 %
 % developed at:  
-%       Laboratoire de Biologie Moléculaire Eucaryote (LBME), 
-%       Centre de Biologie Intégrative (CBI), CNRS; 
+%       Laboratoire de Biologie MolÃ©culaire Eucaryote (LBME), 
+%       Centre de Biologie IntÃ©grative (CBI), CNRS; 
 %       University of Toulouse, UPS; 31062 
 %       Toulouse; France
 %
